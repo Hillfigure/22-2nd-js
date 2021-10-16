e56dc442b3d146e2c5237a08d58e1750ec559eb9
Hillfigure <49096115+Hillfigure@users.noreply.github.com>
2021-10-16 18:53:17 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+console.log("Hallo Winc Academy studenten")
+
+// Calculations
+let multiResult = 1230941 * 1823794;
+let divResult = 637263 / 54;
+/* The calculations above
+are logged out below. */
+console.log(multiResult);
+console.log(divResult);
